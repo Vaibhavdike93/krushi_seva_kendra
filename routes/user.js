1de651c71fc1ab1ffcec03030dd540b0b48aae71
Suraj9404 <jadhavsuraja09@gmail.com>
2025-08-16 09:44:24 +0530

    done
    done

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var exe = require('./conn');
 var router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('user/index');
 });
+
+router.get("/conatct",async function(req,res){
+  var info = await exe("select * from company_info")
+  var paket = {info}
+  res.render("user/contact.ejs",paket)
+})
+router.post("/sendmessage",async function(req,res){
+  var d = req.body;
+  var sql = `INSERT INTO user_message(name, email, phone, subject, message,)VALUSE(?,?,?,?,?)`;
+  var result = await exe(sql,[d.name,d.email,d.phone,d.subject,d.message]);
+  res.send(result);
+})
 
 
 
