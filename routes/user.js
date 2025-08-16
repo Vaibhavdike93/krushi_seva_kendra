c9ccde6580fad96e4bf620d88bdef8f42382a59f
Vaibhavdike93 <vaibhav.dike93@gmail.com>
2025-08-16 11:07:30 +0530

    Import Navbar and footer

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,14 @@ router.get('/', function(req, res) {
   res.render('user/index');
 });
 
+router.get('/login', function(req, res) {
+  res.render('user/signin.ejs');
+});
+
+router.get('/registration', function(req, res) {
+  res.render('user/registration.ejs');
+});
+
 
 
 
